fcd36586c10514d125b6501a2e9bc7f919914664
tony9402 <tony9402@naver.com>
2023-07-24 08:42:46 +0900

    [ADD] 21756 solution

---
+++ solutions/cpp/21756/main.cpp
@@ -0,0 +1,14 @@
+// Copyright [2023] tony9402.
+#include<bits/stdc++.h>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int N; std::cin >> N;
+    int L = 1, R = 2;
+    while (L > N || N >= R) L *= 2, R *= 2;
+    std::cout << L;
+
+    return 0;
+}
